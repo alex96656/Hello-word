2f375050a64f4c60d8623b2ddeb28d00d0372c21
Alex <godwinuzochi51@gmail.com>
2025-08-30 17:15:36 +0000

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,8 @@
+# Stage all new and changed files (test + workflow)
+git add .
+
+# Commit with a clear message
+git commit -m "Add basic pytest test and workflow setup"
+
+# Push to GitHub
+git push
